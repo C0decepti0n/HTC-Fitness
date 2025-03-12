f74a78c72df74c1e7d197500290628550396b557
wlegard <whitleylegard.com>
2025-03-11 20:06:16 -0500

    adding to ReminderCard Component

---
+++ client/src/components/ReminderCard.jsx
@@ -0,0 +1,38 @@
+
+import React from 'react';
+import axios from 'axios'
+
+const ReminderCard = ()=>{
+
+
+
+//axios get request 
+const getReminders = ()=>{
+
+}
+//axios Post
+ const postReminders = () =>{
+
+ }
+
+
+// axios request to update
+const updateReminder = () =>{
+
+}
+
+
+// axios DELETE reminder
+const deleteReminder = () =>{
+
+}
+
+return (
+  <div>
+
+
+  </div>
+)
+
+}
+export default ReminderCard;

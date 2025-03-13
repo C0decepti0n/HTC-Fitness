0b52e6073c4908a37f43ec43e44b5835ad6b619a
wlegard <whitleylegard.com>
2025-03-13 12:23:23 -0500

    working eslint problems

--- client/src/components/ReminderCard.jsx
+++ client/src/components/ReminderCard.jsx
@@ -1,140 +1,134 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-import React, {useState, useEffect} from 'react';
-import axios from 'axios'
-
-const ReminderCard = ()=>{
-// set state to store reminders
-const [ reminders, setReminders] = useState([]);
-
-// set new reminders 
-const[newReminders, setNewReminders] = useState({
-  title: '',
-  description: '',
-  date: ''
-
-});
-
-
-//mounts fetched reminders 
-useEffect(()=>{
-  getReminders();
-}, [])
-
-//axios get request 
-const getReminders = ()=>{
-axios.get('/reminders')
-.then((response)=>{
-setReminders(response.data)
-})
-.catch((err)=>{
-  console.error("Can not get all reminders:", err)
-})
-}
-
-
-//axios Post
- const postReminders = () =>{
-axios.post('/reminders'),{
-  title: newReminders.title,
-  description: newReminders.description,
-  date: newReminders.date,
-  user:userId,
-}
-.then(()=>{
-
-  setReminders([...reminders, response.data])
-  setNewReminders({
+const ReminderCard = () => {
+  // set state to store reminders
+  const [reminders, setReminders] = useState([]);
+  // edit reminders
+  // const [editReminders, setEditReminders] = useEffect();
+  // set new reminders
+  const [newReminders, setNewReminders] = useState({
     title: '',
     description: '',
-    date: ''
-  //userId:????
-  })
-})
-.catch((err)=>{
-console.error("Can not create reminder:", err)
-})
- }
+    date: '',
+  });
 
+  // axios get request
+  const getReminders = () => {
+    axios
+      .get('/reminders')
+      .then((response) => {
+        setReminders(response.data);
+      })
+      .catch((err) => {
+        console.error('Can not get all reminders:', err);
+      });
+  };
+  // mounts fetched reminders
+  useEffect(() => {
+    getReminders();
+  }, []);
 
-// axios request to update
-const updateReminders = (id, updatedReminder) =>{
-axios.patch(`/reminders/${id}`, updatedReminder)
-.then(()=>{
-  getReminders();
-})
-.catch((err)=>{
-  console.error(err)
+  // axios Post
+  const postReminders = () => {
+    axios
+      .post('/reminders', {
+        title: newReminders.title,
+        description: newReminders.description,
+        date: newReminders.date,
+        // user: userId,
+      })
+      .then((response) => {
+        setReminders([...reminders, response.data]);
+        setNewReminders({
+          title: '',
+          description: '',
+          date: '',
+          // userId:????
+        });
+      })
+      .catch((err) => {
+        console.error('Can not create reminder:', err);
+      });
+  };
+  // axios request to update
+  const updateReminders = (id, updatedReminder) => {
+    axios
+      .patch(`/reminders/${id}`, updatedReminder)
+      .then(() => {
+        getReminders();
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
 
-})
-}
+  // axios DELETE reminder
+  const deleteReminders = (id) => {
+    axios
+      .delete(`/reminders/${id}`)
+      .then(() => {
+        getReminders();
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
 
+  // handle input change for the new reminders
+  // const handleInputChange = (e){
+  //   setNewReminders((previousState)=>{
+  // ...
+  //   })
+  // }
 
-// axios DELETE reminder
-const deleteReminders = (id) =>{
-axios.delete(`/reminders/${id}`)
-.then(()=>{
-  getReminders();
-})
-.catch((err)=>{
-  console.error(err)
+  return (
+    <div>
+      <h2>Reminders</h2>
+      <div>
+        {reminders.map((reminder) => (
+            <div key={reminder.id}>
+              <h3>{reminder.title}</h3>
+              <p>{reminder.description}</p>
+              <p>{new Date(reminder.date).toLocaleString()}</p>
+              <button
+                onClick={() => updateReminders(reminder.id, { completed: true })}
+              >
+                Completed!
+              </button>
+              <button onClick={() => deleteReminders(reminder.id)}>
+                Delete
+              </button>
+            </div>
+        ))}
+      </div>
 
-})
-}
+      <div>
+        <h3>Create New Reminder</h3>
+        <input
+          type='text'
+          name='title'
+          placeholder='Title'
+          value={newReminders.title}
+          // onChange={handleInputChange}
+        />
+        <input
+          type='text'
+          name='description'
+          placeholder='Description'
+          value={newReminders.description}
+          // onChange={handleInputChange}
+        />
+        <input
+          type='datetime-local'
+          name='date'
+          value={newReminders.date}
+          // onChange={handleInputChange}
+        />
+        <button onClick={postReminders}>Create Reminder</button>
+      </div>
+    </div>
+  );
+};
 
-
-
-
-
-
-
-return (
-  <div>
-    <h2>Reminders</h2>
-  <div>
-{reminders.map((reminder)=> {
-<div key={reminder._id}>
-<h3>{reminder.title}</h3>
-<p>{reminder.description}</p>
-<p>{new Date(reminder.date).toLocaleString()}</p>
-<button onClick={()=>updateReminders(reminder._id, {completed: true})}>Completed!</button>
-<button onClick={()=>deleteReminders(reminder._id)}>Delete</button>
-
-</div>
-})}
-
-  </div>
-
-
-
-  <div>
-    <h3>Create New Reminder</h3>
-    <input
-    type='text'
-    name='title'
-    placeholder='Ttitle'
-    value={newReminders.title}
-    //onChange={}
-    />
-    <input 
-    type='text'
-    name='description'
-    placeholder='Description'
-    value={newReminders.description}
-    //onChange={}
-
-    />
-    <input
-    type='datetime-local'
-    name='date'
-    placeholder='Description'
-    value={newReminders.date}
-      //onChange={}
-
-    />
-  <button onClick={postReminders}>Create Reminder</button>
-  </div>
-  </div>
-)
-
-}
 export default ReminderCard;
--- server/routes/reminders.js
+++ server/routes/reminders.js
@@ -4,66 +4,59 @@ const { Reminder } = require('../db/Reminder');
 const router = express.Router();
 
 // GET: fetch all reminders in the database
-router.get('/:userId', (req, res)=>{
-Reminder.find({})
-.then((reminders)=>{
-res.status(200).send(reminders)
-})
-.catch((err)=>{
-console.error('Failure to find reminders:', err);
-res.sendStatus(500)
-})
-})
+router.get('/:userId', (req, res) => {
+  Reminder.find({})
+    .then((reminders) => {
+      res.status(200).send(reminders);
+    })
+    .catch((err) => {
+      console.error('Failure to find reminders:', err);
+      res.sendStatus(500);
+    });
+});
 
-
-//POST: create new reminders
-router.post('/', (req, res)=>{
-  
+// POST: create new reminders
+router.post('/', (req, res) => {
   Reminder.create(req.body)
-  .then(()=>{
-    res.sendStatus(201)
-  })
-  .catch((err)=>{
-    console.error('Failure to create reminder:', err)
-    res.sendStatus(500);
-  })
-})
+    .then(() => {
+      res.sendStatus(201);
+    })
+    .catch((err) => {
+      console.error('Failure to create reminder:', err);
+      res.sendStatus(500);
+    });
+});
 
-
-
-//UPDATE
-router.patch('/:id', (req, res)=>{
+// UPDATE
+router.patch('/:id', (req, res) => {
   Reminder.findByIdAndUpdate(req.params.id, req.body)
-  .then((updateReminder)=>{
-    if(updateReminder){
-      res.status(200).send(updateReminder)
-    }else{
-      res.sendStatus(404)
-    }
-  })
-  .catch ((err)=>{
-    console.error('Failure to update reminder:', err)
-    res.sendStatus(500)
-  })
-})
+    .then((updateReminder) => {
+      if (updateReminder) {
+        res.status(200).send(updateReminder);
+      } else {
+        res.sendStatus(404);
+      }
+    })
+    .catch((err) => {
+      console.error('Failure to update reminder:', err);
+      res.sendStatus(500);
+    });
+});
 
-
-
-//DELETE
-router.delete('/:id', (req, res)=>{
+// DELETE
+router.delete('/:id', (req, res) => {
   Reminder.findByIdAndDelete(req.params.id)
-  .then((delReminder)=>{
-    if(delReminder){
-      res.sendStatus(200)
-    }else{
-      console.error("Failure to find reminder:")
-      res.sendStatus(404)
-    }
-  
-  })
-  .catch ((err)=>{
-    console.error('Failure to delete reminder:', err)
-    res.sendStatus(500)
-  })
-})
+    .then((delReminder) => {
+      if (delReminder) {
+        res.sendStatus(200);
+      } else {
+        console.error('Failure to find reminder:');
+        res.sendStatus(404);
+      }
+    })
+    .catch((err) => {
+      console.error('Failure to delete reminder:', err);
+      res.sendStatus(500);
+    });
+});
 module.exports = router;
